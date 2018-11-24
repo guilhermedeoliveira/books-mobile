035dba0d71217d0c2caced4e36f00927e3624dc2
guilhermedeoliveira <contatoguilhermedeoliveira@gmail.com>
2018-11-24 00:00:41 -0300

    init 'ListScreen' design structure

---
+++ src/components/Header/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { node, string } from 'prop-types';
+import styled from 'styled-components/native';
+
+import Text from '../Text';
+import { em } from '../../styles';
+
+const Header = ({ left, title, right }) => (
+  <HeaderWrapper>
+    <HeaderContainer>
+      {left}
+      <Text large>{title}</Text>
+      {right}
+    </HeaderContainer>
+  </HeaderWrapper>
+);
+
+const HeaderWrapper = styled.View`
+  paddingHorizontal: ${em(0.3)};
+`;
+
+const HeaderContainer = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+Header.propTypes = {
+  left: node.isRequired,
+  title: string.isRequired,
+  right: node.isRequired
+};
+
+export default Header;
--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,15 +1,52 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Divider } from 'react-native-elements';
 
-import { ViewContainer } from '../components/shared';
+import { ViewContainer, StyledMaterialIcon } from '../components/shared';
+import Header from '../components/Header';
+import Grid from '../components/Grid';
+
+import styles, { em, isIOS } from '../styles';
+import { listScreendividerStyle } from '../styles/general';
 
 class ListScreen extends Component {
-  state = {};
+  static navigationOptions = {
+    drawerLabel: 'List'
+  };
+
+  state = {
+    books: [
+      { id: 'dasd', name: 'opa' },
+      { id: 'dasdd', name: 'opa' },
+      { id: 'dad', name: 'opa' },
+      { id: 'dsd', name: 'opa' },
+      { id: 'dsdasd', name: 'opa' },
+      { id: 'dadasd', name: 'opa' },
+      { id: 'daadd', name: 'opa' },
+      { id: 'dafgsd', name: 'opa' },
+      { id: 'dashgd', name: 'opa' },
+      { id: 'aaadashgd', name: 'opa' }
+    ]
+  };
 
   render() {
     return (
-      <ViewContainer centralized>
-        <Text>List</Text>
+      <ViewContainer
+        paddingVertical={isIOS ? em(3.5) : em(1.5)}
+        paddingHorizontal={em(0.3)}
+        backgroundColor={styles.colors.mainColor}
+      >
+        <Header
+          left={<StyledMaterialIcon name="menu" />}
+          title="Design Books"
+          right={<StyledMaterialIcon name="search" />}
+        />
+
+        <Divider style={listScreendividerStyle} />
+
+        <Grid
+          data={this.state.books}
+          grid={3}
+        />
       </ViewContainer>
     );
   }
